00a64c3310c08d398361c379fd0a662c44a4956b
warlinux <warlinux@yandex.ru>
2021-03-07 02:26:26 +0300

    change

--- ip.py
+++ ip.py
@@ -1,23 +1,5 @@
-import bs4
-import requests
-
-s = requests.get('https://2ip.ua/ru/')
-b = bs4.BeautifulSoup(s.text, "html.parser")
-a = b.select(" .ipblockgradient .ip")[0].getText()
-#print(a)
-# получение ip-адреса
 import urllib.request
 import re
+
 res = urllib.request.urlopen('http://2ip.ru/').read()
 print(re.search(b'\d+\.\d+\.\d+\.\d+', res).group())
-
-
-
-
-
-
-
-
-
-
-
