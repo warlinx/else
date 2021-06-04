9607e3af8d158c7e20e15f50a1b2d088b1c89189
warlinux <warlinux@yandex.ru>
2021-06-05 01:27:25 +0300

    some tasks

--- store/YouTube.py
+++ store/YouTube.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
 import youtube_dl
-import urllib
-import shutil
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['https://www.youtube.com/watch?v=sG6rrIxY9q0'])
